356ecf4e0834277aadb8880cf98f84c00f60b1be
qwas4689 <qwas4689@gmail.com>
2023-01-23 03:15:17 +0900

    [Feat] 드래그앤 드랍 완료 및 머지 구현 테스트

--- Merge_Idle Game/Assets/Scripts/ClickAndDrop.cs
+++ Merge_Idle Game/Assets/Scripts/ClickAndDrop.cs
@@ -6,7 +6,7 @@ using UnityEngine.EventSystems;
 public class ClickAndDrop : MonoBehaviour
 {
     private List<GameObject> _weaponSetActiveTrue = new List<GameObject>();
-    private Vector2 _weaponPos;
+    private Vector2 _touchPos;
     private Touch _touch;
     private GameObject _select;
 
@@ -15,19 +15,22 @@ public class ClickAndDrop : MonoBehaviour
     private const float Y_POS_MIN = 350f;
     private const float Y_POS_MAX = 1150f;
 
-    void Update()
+    private void Update()
     {
         if (Input.touchCount > 0)
         {
-             _touch = Input.GetTouch(0);
-            _weaponPos = Camera.main.ScreenToWorldPoint(_touch.position);
+            // 1회 터치만 받음
+            _touch = Input.GetTouch(0);
+            // 터치한 곳을 월드좌표로 받아 옴
+            _touchPos = Camera.main.ScreenToWorldPoint(_touch.position);
 
             // 지금 UI가 생성 할 수 있는 UI면 이거 실행 아니면 return
 
-            
+
             // 무기가 있을 수 있는 위치만 터지를 받음
             if (CanMoveArea(_touch))
             {
+                // 터치 눌렀을 때
                 if (_touch.phase == TouchPhase.Began)
                 {
                     for (int i = 0; i < ObjectPool.Instance.WeaponPool.Count; ++i)
@@ -38,50 +41,65 @@ public class ClickAndDrop : MonoBehaviour
                         }
                         else
                         {
+                            // 활성화 되어 있는 무기만 리스트에 추가
                             _weaponSetActiveTrue.Add(ObjectPool.Instance.WeaponPool[i]);
                         }
                     }
 
-                    float offset = 0.1f;
-
+                    // 무기를 터치했는지 확인
                     for (int i = 0; i < _weaponSetActiveTrue.Count; ++i)
                     {
-                        if (_weaponPos.x - offset < _weaponSetActiveTrue[i].transform.position.x && _weaponSetActiveTrue[i].transform.position.x > _weaponPos.x - offset && _weaponPos.y - offset < _weaponSetActiveTrue[i].transform.position.y && _weaponSetActiveTrue[i].transform.position.y > _weaponPos.y - offset)
+                        if (TouchWeapon(_touchPos, i))
                         {
                             _select = _weaponSetActiveTrue[i];
                             break;
                         }
-                        else
-                        {
-                            continue;
-                        }
                     }
+
 
                     EndTouch();
                 }
 
+                // 드래그 상태
                 if (_touch.phase == TouchPhase.Moved)
                 {
+                    // 영역을 벗어나면 드래그상태 풀림
                     if (!CanMoveArea(_touch))
                     {
                         EndTouchAndClear();
-                        return;
                     }
+
+                    // 선택된게 있을때만 움직임
                     if (_select != null)
                     {
-                        _select.transform.position = _weaponPos;
+                        _select.transform.position = _touchPos;
                     }
                 }
 
+                // 터치 땠을 때
                 if (_touch.phase == TouchPhase.Ended)
                 {
+                    // 땠을 때 선택된 것과 인접한 위치에 무기가 있으면 머지 함
+                    for (int i = 0; i < _weaponSetActiveTrue.Count; ++i)
+                    {
+                        if (TouchWeapon(_select.transform.position, i))
+                        {
+                            ObjectPool.Instance.WeaponPool[i].SetActive(false);
+                            ++_select.GetComponent<Weapon>().WeaponLevel;
+                        }
+                    }
+
+                    // 셀렉트의 무기 인덱스를 하나 올려주고
+                    // 그것과 인접한 것을 셋엑티프 false 로 한다
+
+                    // 초기화 및 리스트 초기화
                     EndTouchAndClear();
                 }
             }
             else
             {
+                // 초기화
                 Init();
-                return;
             }
         }
     }
@@ -92,24 +110,40 @@ public class ClickAndDrop : MonoBehaviour
     }
 
     /// <summary>
-    /// 초기화
+    /// 터치한 위치 및 리스트 초기화 
     /// </summary>
     private void Init()
     {
-        _weaponPos = Vector2.zero;
+        _touchPos = Vector2.zero;
         _weaponSetActiveTrue.Clear();
     }
 
+    /// <summary>
+    /// 선택한 무기가 클릭한 위치에 인접하면 true 아니면 false
+    /// </summary>
+    /// <param name="i">리스트의 인덱스</param>
+    /// <returns></returns>
+    private bool TouchWeapon(Vector2 touchPos, int i)
+    {
+        float offset = 0.1f;
 
+        return touchPos.x - offset < _weaponSetActiveTrue[i].transform.position.x && _weaponSetActiveTrue[i].transform.position.x < touchPos.x + offset && touchPos.y - offset < _weaponSetActiveTrue[i].transform.position.y && _weaponSetActiveTrue[i].transform.position.y < touchPos.y + offset;
+    }
+
+    /// <summary>
+    /// 클릭했을 때 선택된 무기가 없으면 호출
+    /// </summary>
     private void EndTouch()
     {
         if (_select == null)
         {
             Init();
-            return;
         }
     }
 
+    /// <summary>
+    /// 터치가 끝났을 때 호출
+    /// </summary>
     private void EndTouchAndClear()
     {
         Init();
--- Merge_Idle Game/Assets/Scripts/Weapon.cs
+++ Merge_Idle Game/Assets/Scripts/Weapon.cs
@@ -2,16 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D;
+using UnityEngine.Events;
 
 public class Weapon : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private SpriteAtlas _spriteAtlas;
 
+    public int WeaponLevel { get; set; }
+    private int _originalLevel;
+
     private const string WEAPON = "Weapon_";
 
     void Start()
     {
+        _originalLevel = Ability.Instance.WeaponLevel;
+        WeaponLevel = _originalLevel;
+
         _spriteRenderer.sprite = _spriteAtlas.GetSprite(WEAPON + Ability.Instance.WeaponLevel);
     }
+
+    private void Update()
+    {
+        if (WeaponLevel != _originalLevel)
+        {
+            _spriteRenderer.sprite = _spriteAtlas.GetSprite(WEAPON + WeaponLevel);
+            ++_originalLevel;
+        }
+    }
+
+    // 어빌리티로 최소 무기 레벨을 올리면 무기의 스프라이트를 바꿔줘야 함 
 }
