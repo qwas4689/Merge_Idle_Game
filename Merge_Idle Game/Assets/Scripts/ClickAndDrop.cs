5fb66e5282b660a7ad3b8705afab8f6557ffdc4d
qwas4689 <qwas4689@gmail.com>
2023-01-23 02:23:27 +0900

    [Feat] 그래그앤드롭 구현 중

--- Merge_Idle Game/Assets/Scripts/ClickAndDrop.cs
+++ Merge_Idle Game/Assets/Scripts/ClickAndDrop.cs
@@ -7,38 +7,112 @@ public class ClickAndDrop : MonoBehaviour
 {
     private List<GameObject> _weaponSetActiveTrue = new List<GameObject>();
     private Vector2 _weaponPos;
+    private Touch _touch;
+    private GameObject _select;
+
+    private const float X_POS_MIN = 10f;
+    private const float X_POS_MAX = 1070f;
+    private const float Y_POS_MIN = 350f;
+    private const float Y_POS_MAX = 1150f;
 
     void Update()
     {
         if (Input.touchCount > 0)
         {
-            Touch touch = Input.GetTouch(0);
+             _touch = Input.GetTouch(0);
+            _weaponPos = Camera.main.ScreenToWorldPoint(_touch.position);
 
-            if (touch.phase == TouchPhase.Began)
+            // 지금 UI가 생성 할 수 있는 UI면 이거 실행 아니면 return
+
+            
+            // 무기가 있을 수 있는 위치만 터지를 받음
+            if (CanMoveArea(_touch))
             {
-
-                for (int i = 0; i < ObjectPool.Instance.WeaponPool.Count; ++i)
+                if (_touch.phase == TouchPhase.Began)
                 {
-                    if (ObjectPool.Instance.WeaponPool[i].activeSelf == false)
+                    for (int i = 0; i < ObjectPool.Instance.WeaponPool.Count; ++i)
                     {
-                        break;
+                        if (ObjectPool.Instance.WeaponPool[i].activeSelf == false)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            _weaponSetActiveTrue.Add(ObjectPool.Instance.WeaponPool[i]);
+                        }
                     }
-                    else
+
+                    float offset = 0.1f;
+
+                    for (int i = 0; i < _weaponSetActiveTrue.Count; ++i)
                     {
-                        _weaponSetActiveTrue.Add(ObjectPool.Instance.WeaponPool[i]);
+                        if (_weaponPos.x - offset < _weaponSetActiveTrue[i].transform.position.x && _weaponSetActiveTrue[i].transform.position.x > _weaponPos.x - offset && _weaponPos.y - offset < _weaponSetActiveTrue[i].transform.position.y && _weaponSetActiveTrue[i].transform.position.y > _weaponPos.y - offset)
+                        {
+                            _select = _weaponSetActiveTrue[i];
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+
+                    EndTouch();
+                }
+
+                if (_touch.phase == TouchPhase.Moved)
+                {
+                    if (!CanMoveArea(_touch))
+                    {
+                        EndTouchAndClear();
+                        return;
+                    }
+                    if (_select != null)
+                    {
+                        _select.transform.position = _weaponPos;
                     }
                 }
-            }
 
-            if (touch.phase == TouchPhase.Moved)
-            {
-                
+                if (_touch.phase == TouchPhase.Ended)
+                {
+                    EndTouchAndClear();
+                }
             }
-
-            if (touch.phase == TouchPhase.Ended)
+            else
             {
-                _weaponSetActiveTrue.Clear();
+                Init();
+                return;
             }
         }
+    }
+
+    private bool CanMoveArea(Touch touchPos)
+    {
+        return touchPos.position.x < X_POS_MAX && touchPos.position.x > X_POS_MIN && touchPos.position.y < Y_POS_MAX && touchPos.position.y > Y_POS_MIN;
+    }
+
+    /// <summary>
+    /// 초기화
+    /// </summary>
+    private void Init()
+    {
+        _weaponPos = Vector2.zero;
+        _weaponSetActiveTrue.Clear();
+    }
+
+
+    private void EndTouch()
+    {
+        if (_select == null)
+        {
+            Init();
+            return;
+        }
+    }
+
+    private void EndTouchAndClear()
+    {
+        Init();
+        _select = null;
     }
 }
